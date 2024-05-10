b3ad6b66f0c2faf0a8b00a464f0db31cfa206ac7
Zarconos <119136113+Zarconos@users.noreply.github.com>
2024-05-10 11:25:15 +0200

    cards url updated

--- src/data/Card.jsx
+++ src/data/Card.jsx
@@ -1,17 +1,16 @@
 // Card.jsx
 
 import PropTypes from 'prop-types';
-
+import cardsData from './cardsData';
 
 const Card = ({ cardNumber, color, onClick }) => {
-    let imagePath = '../assets/cards/back card/back_card.png'; // Chemin d'accès par défaut ou vide
-    if (cardNumber !== undefined && color !== undefined) {
-        if (color === 'red') {
-            imagePath = `../assets/cards/${cardNumber}-r.png`;
-        } else if (color === 'blue') {
-            imagePath = `../assets/cards/${cardNumber}-b.png`;
-        }
-    }
+    // Crée une fonction qui récupère l'URL de l'image en fonction de la couleur et du numéro de la carte
+    const getImagePath = (color, cardNumber) => {
+        const card = cardsData.find(card => card.color === color && card.value === cardNumber);
+        return card ? card.image : 'https://i.imgur.com/orp60NC.png'; // Image par défaut si aucune correspondance n'est trouvée
+    };
+
+    const imagePath = getImagePath(color, cardNumber);
 
     return (
         <img
@@ -19,7 +18,7 @@ const Card = ({ cardNumber, color, onClick }) => {
             alt={`Card ${cardNumber}`}
             onClick={onClick}
             style={{
-                cursor: onClick ? 'pointer' : 'pointer',               
+                cursor: onClick ? 'pointer' : 'pointer',
                 filter: onClick ? 'brightness(1.1)' : 'brightness(1)',
                 transition: 'filter 0.3s ease',
             }}
@@ -27,11 +26,11 @@ const Card = ({ cardNumber, color, onClick }) => {
     );
 };
 
-
 Card.propTypes = {
     cardNumber: PropTypes.number,
     color: PropTypes.string,
     onClick: PropTypes.func.isRequired,
 };
-  
+
 export default Card;
+
--- src/data/cardsData.jsx
+++ src/data/cardsData.jsx
@@ -1,26 +1,26 @@
 // cardsData.jsx
 
 const cardsData = [
-    { id: 1, name: "1", value: 1, color: "red", image: "1-r.png" },
-    { id: 2, name: "2", value: 2, color: "red", image: "2-r.png" },
-    { id: 3, name: "3", value: 3, color: "red", image: "3-r.png" },
-    { id: 4, name: "4", value: 4, color: "red", image: "4-r.png" },
-    { id: 5, name: "5", value: 5, color: "red", image: "5-r.png" },
-    { id: 6, name: "6", value: 6, color: "red", image: "6-r.png" },
-    { id: 7, name: "7", value: 7, color: "red", image: "7-r.png" },
-    { id: 8, name: "8", value: 8, color: "red", image: "8-r.png" },
-    { id: 9, name: "9", value: 9, color: "red", image: "9-r.png" },
-    { id: 10, name: "10", value: 10, color: "red", image: "10-r.png" },
-    { id: 11, name: "1", value: 1, color: "blue", image: "1-b.png" },
-    { id: 12, name: "2", value: 2, color: "blue", image: "2-b.png" },
-    { id: 13, name: "3", value: 3, color: "blue", image: "3-b.png" },
-    { id: 14, name: "4", value: 4, color: "blue", image: "4-b.png" },
-    { id: 15, name: "5", value: 5, color: "blue", image: "5-b.png" },
-    { id: 16, name: "6", value: 6, color: "blue", image: "6-b.png" },
-    { id: 17, name: "7", value: 7, color: "blue", image: "7-b.png" },
-    { id: 18, name: "8", value: 8, color: "blue", image: "8-b.png" },
-    { id: 19, name: "9", value: 9, color: "blue", image: "9-b.png" },
-    { id: 20, name: "10", value: 10, color: "blue", image: "10-b.png" }
+    { id: 1, name: "1", value: 1, color: "red", image: "https://i.imgur.com/ooTBhyF.png" },
+    { id: 2, name: "2", value: 2, color: "red", image: "https://i.imgur.com/WaJSeEk.png" },
+    { id: 3, name: "3", value: 3, color: "red", image: "https://i.imgur.com/ZYUjoz1.png" },
+    { id: 4, name: "4", value: 4, color: "red", image: "https://i.imgur.com/c4oTs7m.png" },
+    { id: 5, name: "5", value: 5, color: "red", image: "https://i.imgur.com/HO91d7i.png" },
+    { id: 6, name: "6", value: 6, color: "red", image: "https://i.imgur.com/EjRs8g4.png" },
+    { id: 7, name: "7", value: 7, color: "red", image: "https://i.imgur.com/kQCNyfB.png" },
+    { id: 8, name: "8", value: 8, color: "red", image: "https://i.imgur.com/GqIDqEX.png" },
+    { id: 9, name: "9", value: 9, color: "red", image: "https://i.imgur.com/jxHSMua.png" },
+    { id: 10, name: "10", value: 10, color: "red", image: "https://i.imgur.com/PVksnbU.png" },
+    { id: 11, name: "1", value: 1, color: "blue", image: "https://i.imgur.com/aCC4nzI.png" },
+    { id: 12, name: "2", value: 2, color: "blue", image: "https://i.imgur.com/VfZZwNs.png" },
+    { id: 13, name: "3", value: 3, color: "blue", image: "https://i.imgur.com/3SHzqKF.png" },
+    { id: 14, name: "4", value: 4, color: "blue", image: "https://i.imgur.com/QwpyWSb.png" },
+    { id: 15, name: "5", value: 5, color: "blue", image: "https://i.imgur.com/lq7txi3.png" },
+    { id: 16, name: "6", value: 6, color: "blue", image: "https://i.imgur.com/YRKtqCJ.png" },
+    { id: 17, name: "7", value: 7, color: "blue", image: "https://i.imgur.com/R3KELUV.png" },
+    { id: 18, name: "8", value: 8, color: "blue", image: "https://i.imgur.com/GqIDqEX.png" },
+    { id: 19, name: "9", value: 9, color: "blue", image: "https://i.imgur.com/avKf2VQ.png" },
+    { id: 20, name: "10", value: 10, color: "blue", image: "https://i.imgur.com/Cj6YmEf.png" }
   ];
   
   export default cardsData;
